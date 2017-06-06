6d27fa0b09d068d6433802fa950ebf6bf7c01e6d
Zadd7 <coder_zadd7@163.com>
2017-06-06 21:18:51 +0800

    完成

--- src/components/orderSearch.vue
+++ src/components/orderSearch.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="main-wrapper">
         <div class="tab">
-            <div class="tab-item" @click="">时间</div>
-            <div class="tab-item" @click="">历史</div>
+            <div class="tab-item" :class="{'tab-active':timeActiveFlag}" @click="timeActive">时间</div>
+            <div class="tab-item" :class="{'tab-active':historyActiveFlag}" @click="historyActive">历史</div>
         </div>
         <div class="select-wrapper">
             <div class="time-wrapper">
@@ -62,10 +62,14 @@
 export default {
     data() {
             return {
+                timeActiveFlag:true,
+                historyActiveFlag:false,
                 year: '',
                 month: '',
                 date: '',
                 noonData: '',
+                testData:'',
+                payData:'',
                 noons: [{
                     'value': '上午',
                     'label': '上午'
@@ -75,6 +79,16 @@ export default {
                 }]
             }
         },
+        methods:{
+            timeActive(){
+                this.timeActiveFlag = true;
+                this.historyActiveFlag = false;
+            },
+            historyActive(){
+                this.timeActiveFlag = false;
+                this.historyActiveFlag = true;
+            }
+        }
 
 }
 </script>
@@ -88,17 +102,8 @@ export default {
 	display: flex;
 	margin:0 -8px 0 -8px;
 }
-.main-wrapper .select-wrapper{
-	text-align: left;
-}
 
-.select-wrapper .title{
-	display: inline-block;
-	padding: 10px 0 10px 0;
-}
-.main-wrapper .time-content{
-	display: flex;
-}
+
 .main-wrapper .tab .tab-item{
 	display: inline-block;
 	flex: 1;
@@ -106,6 +111,21 @@ export default {
 	padding: 8px;
 	padding-top: 0;
 	border-bottom: 1px solid #E5E5E7;
+}
+.main-wrapper .tab .tab-item.tab-active{
+    border-bottom: 1px solid #DF2D8F;
+}
+.main-wrapper .select-wrapper{
+    text-align: left;
+}
+
+.select-wrapper .title{
+    display: inline-block;
+    padding: 10px 0 10px 0;
+}
+.main-wrapper .time-content{
+    display: flex;
+    justify-content: center;
 }
 .time-content .time-item + .time-item{
 	margin-left: 5px;
@@ -120,5 +140,7 @@ export default {
 	width: 100%;
 	height: 50px;
 	margin-top: 30px;
+    background-color: #DF2D8F;
+    border:none;
 }
 </style>
